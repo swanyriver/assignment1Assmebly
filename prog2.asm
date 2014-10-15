2526b1d9816bde9ba4a0349359d327137ac8a808
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-10-15 16:59:39 -0700

    intro procedure completed

--- prog2.asm
+++ prog2.asm
@@ -9,15 +9,15 @@ INCLUDE Irvine32.inc
 ;////////////////PROGRAM CONSTANTS//////////////////
 LOWER_BOUND = 0     ;input must be greater than this
 UPPER_BOUND = 47    ;input must be less than this
-STRING_MAX  = 40    ;maximum length of user input strings
+;STRING_MAX  = 40    ;maximum length of user input strings
+STRING_MAX  = 8    ;maximum length of user input strings
 
 .data
 
 ;//////////////STRINGS///////////////////////////////
 intro_s         BYTE    "Fibonacci Numbers",13,10
                 BYTE    "Programed by Brandon Swanson",13,10,0
-namePrompt_s    BYTE    "What is your name",0
-welcome_s       BYTE    "Welcome to Fibonacci Numbers ",13,10,0
+namePrompt_s    BYTE    "What is your name: ",0
 instruction_s   BYTE    "You will enter the number of Fibonacci terms you would like displayed",13,10
                 BYTE    "Enter a whole number between 1 and 46 inclusive",13,10,13,10,0
 mumPrompt_s     BYTE    "How many terms would you like to see: ",0
@@ -25,13 +25,15 @@ farewell_s      BYTE    "Thank you for using my program" ,13,10
                 BYTE    "-Brandon",13,10,0
 
 ;/////////////ERROR STRINGS//////////////////////////
-nameTooLong_s   BYTE    "The name you entered was too long", 13,10 
+noNameEntered   BYTE    "The name you entered was too long", 13,10 
                 BYTE    "less than 40 characters please",13,10,0
-outOfRange_s    BYTE    "please keep your input greater than 0 and less than 47",13,10,0
+outOfRange_s    BYTE    "nothing was entered, please try again",13,10,0
 
 
 ;/////////////USER INPUT VARIABLES//////////////////
-userName        BYTE    STRING_MAX dup(?)
+welcome_s       BYTE    "Welcome to Fibonacci Numbers "     
+                ;user name will become concatenated with welcome string
+userName        BYTE    STRING_MAX+1 dup(?)
 nTerms          BYTE    ?
 
 ;////////////PROGRAM INFORMATION///////////////////
@@ -54,7 +56,7 @@ main ENDP
 ;Returns:   Users name stored in userName buffer
 ;
 ;#################################################
-Intro PROC USES edx
+Intro PROC USES edx eax ecx
     
     ;//print intro string//
     mov edx, OFFSET intro_s
@@ -66,15 +68,28 @@ Intro PROC USES edx
 
 getName:
     ;//get user name//
+    mov edx, OFFSET userName
+    mov ecx, STRING_MAX
+    call ReadString
 
     ;//verify input//
+    test eax,eax
+    je noInput      ;if(eax==0) no string entered
+    jmp Welcome     ;else continue program
 
+noInput:
     ;//print error msg//
+    mov edx, OFFSET noNameEntered
+    call WriteString
+
+    jmp getName
 
 welcome:
     ;//print user welcome//
-
-
+    mov edx, OFFSET welcome_s
+    call WriteString
+    call crlf
+    
     
     ret
 Intro ENDP
