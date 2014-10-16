a38026795075a6e63e0147f210ce035253e87f9d
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-10-15 17:26:26 -0700

    instruction procedure written

--- prog2.asm
+++ prog2.asm
@@ -9,8 +9,7 @@ INCLUDE Irvine32.inc
 ;////////////////PROGRAM CONSTANTS//////////////////
 LOWER_BOUND = 0     ;input must be greater than this
 UPPER_BOUND = 47    ;input must be less than this
-;STRING_MAX  = 40    ;maximum length of user input strings
-STRING_MAX  = 8    ;maximum length of user input strings
+STRING_MAX  = 40    ;maximum length of user input strings
 
 .data
 
@@ -18,20 +17,19 @@ STRING_MAX  = 8    ;maximum length of user input strings
 intro_s         BYTE    "Fibonacci Numbers",13,10
                 BYTE    "Programed by Brandon Swanson",13,10,0
 namePrompt_s    BYTE    "What is your name: ",0
-instruction_s   BYTE    "You will enter the number of Fibonacci terms you would like displayed",13,10
+instruction_s   BYTE    13,10,"You will enter the number of Fibonacci terms you would like displayed",13,10
                 BYTE    "Enter a whole number between 1 and 46 inclusive",13,10,13,10,0
-mumPrompt_s     BYTE    "How many terms would you like to see: ",0
-farewell_s      BYTE    "Thank you for using my program" ,13,10
+mumPrompt_s     BYTE    13,10,"How many terms would you like to see: ",0
+farewell_s      BYTE    13,10,"Thank you for using my program" ,13,10
                 BYTE    "-Brandon",13,10,0
 
 ;/////////////ERROR STRINGS//////////////////////////
-noNameEntered   BYTE    "The name you entered was too long", 13,10 
-                BYTE    "less than 40 characters please",13,10,0
-outOfRange_s    BYTE    "nothing was entered, please try again",13,10,0
+noNameEntered   BYTE    "nothing was entered, please try again",13,10,0
+outOfRange_s    BYTE    "please keep your input greater than 0 and less than 47",13,10,0
 
 
 ;/////////////USER INPUT VARIABLES//////////////////
-welcome_s       BYTE    "Welcome to Fibonacci Numbers "     
+welcome_s       BYTE    13,10,"Welcome to Fibonacci Numbers "     
                 ;user name will become concatenated with welcome string
 userName        BYTE    STRING_MAX+1 dup(?)
 nTerms          BYTE    ?
@@ -41,7 +39,11 @@ nTerms          BYTE    ?
 
 .code
 main PROC
+
     call Intro
+    call Instruct
+
+
 
     exit    ; exit to operating system
 main ENDP
@@ -51,7 +53,7 @@ main ENDP
 ;#################################################
 ;PROCEDURE:     Intro
 ;
-;Purpose:   print introduction and get users name
+;Purpose:   print introduction and get user's name
 ;Recieves:  none
 ;Returns:   Users name stored in userName buffer
 ;
@@ -93,6 +95,25 @@ welcome:
     
     ret
 Intro ENDP
+
+
+
+;#################################################
+;PROCEDURE:     Instructions 
+;
+;Purpose:   print instructions for user
+;Recieves:  none
+;Returns:   none
+;
+;#################################################
+Instruct PROC USES edx
+    
+    ;//display instructions//
+    mov edx, OFFSET instruction_s
+    call WriteString
+
+    ret
+Instruct ENDP 
 
 
 END main
